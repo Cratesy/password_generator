777fc8908e460429636e09a8eaced1e4484db65b
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-03-13 17:13:19 +0000

    added arrays js

--- script.js
+++ script.js
@@ -1,21 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// array for all the password charater types
+const numbersarray = ["0", "1", "2", "3", "4", "5", "6", "7", "6", "8", "9"];
 
-function getRandomLower() {
-  return;
-}
+const specialchararray = [`@%+\\/'!#$^?:,)(}{][~-_.`];
 
-function getRandomUpper() {
-  return;
-}
+const uppercasearray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "U",
+  "R",
+  "S",
+  "T",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const lowercasearray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "u",
+  "r",
+  "s",
+  "t",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
-function getRandomNumber() {
-  return;
-}
-function getRandomSymbol() {
-  const symbols = [`@%+\\/'!#$^?:,)(}{][~-_.`];
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
 // Declare a new function on line 3
 function generatePassword() {
   // Create a variable to store our generated password
